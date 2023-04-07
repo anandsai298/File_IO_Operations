0be0841fa402ea348c742ec8f63c7a3c0641cfb8
anandsaikumar <anandsai298@gmail.com>
2023-04-07 09:43:38 +0530

    [sai] Add : FileReadAllLines,FileReadAllText,FileCopy,FileDelete,FileStreamReader,FileStreamWriter

---
+++ File_IO_Operations/File_IO_Operations/Operation.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_IO_Operations
+{
+    public class Operation
+    {
+        public void FileExists(string path)
+        {
+            if (File.Exists(path))
+                Console.WriteLine("file exists");
+            else
+                Console.WriteLine("file not exists");
+        }
+        public void FileReadAllLines(string path)
+        {
+            if (File.Exists(path))
+            {
+                string[] lines=File.ReadAllLines(path);
+                foreach(var data in lines)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+            else
+                Console.WriteLine("file not exists");
+        }
+        public void FileReadAllText(string path)
+        {
+            if (File.Exists(path))
+            {
+                string lines = File.ReadAllText(path);
+                Console.WriteLine(lines);
+            }
+            else
+                Console.WriteLine("file not exists");
+        }
+        public void FileCopy(string path,string NewFile)
+        {
+            if (File.Exists(path))
+            {
+                File.Copy(path, NewFile);
+            }
+            else
+                Console.WriteLine("file not exists");
+        }
+        public void FileDelete(string NewFile)
+        {
+            if (File.Exists(NewFile))
+            {
+                File.Delete(NewFile);
+            }
+            else
+                Console.WriteLine("file not exists");
+        }
+        public void FileStreamReader(string path)
+        {
+            if(File.Exists(path))
+            {
+                using(StreamReader sr =File.OpenText(path))
+                {
+                    while(sr.ReadLine()!=null)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+                }
+            }
+            else
+                Console.WriteLine("file not exists");
+        }
+        public void FileStreamWriter(string path)
+        {
+            if (File.Exists(path))
+            {
+                using (StreamWriter sr = File.AppendText(path))
+                {
+                    string word = "telusa manasa";
+                    sr.WriteLine(word);
+                    sr.Close();
+                    Console.WriteLine(File.ReadAllText(path));
+                }
+            }
+            else
+                Console.WriteLine("file not exists");
+        }
+    }
+}
---
+++ File_IO_Operations/File_IO_Operations/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace File_IO_Operations;
+class program
+{
+    static void Main(string[] args)
+    {
+        Operation op= new Operation();
+        op.FileExists(@"F:\File_IO_Operations\File_IO_Operations\File_IO_Operations\TextFile1.txt");
+        Console.WriteLine("FileReadAllLines");
+        op.FileReadAllLines(@"F:\File_IO_Operations\File_IO_Operations\File_IO_Operations\TextFile1.txt");
+        Console.WriteLine("FileReadAllText");
+        op.FileReadAllText(@"F:\File_IO_Operations\File_IO_Operations\File_IO_Operations\TextFile1.txt");
+        Console.WriteLine("FileCopy");
+        op.FileCopy(@"F:\File_IO_Operations\File_IO_Operations\File_IO_Operations\TextFile1.txt","bheemnayaka");
+        Console.WriteLine("FileDelete");
+        op.FileDelete("wviwbviybviwbvi");
+        Console.WriteLine("FileStreamReader");
+        op.FileStreamReader(@"F:\File_IO_Operations\File_IO_Operations\File_IO_Operations\TextFile1.txt");
+        Console.WriteLine("FileStreamWriter");
+        op.FileStreamWriter(@"F:\File_IO_Operations\File_IO_Operations\File_IO_Operations\TextFile1.txt");
+    }
+}
+
